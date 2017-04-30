6ac5d8329d6b970b351434245136306dec32bb18
David <munsondd@colorado.edu>
2017-04-30 05:16:14 -0600

    benchmarking for app

--- AlcesAlgoBenchmark/benchmark2.py
+++ AlcesAlgoBenchmark/benchmark2.py
@@ -14,57 +14,57 @@ for filename in os.listdir(directory):
     false_positives = 0
     true_positives = 0
     false_negatives = 0
-    skip = 0
+
     if filename.endswith(".txt"):
         # rd is now specified as len?
         rd = int(re.search('_len_(.+?)_r', filename).group(1))
         working_file = open(directory +"/"+filename, 'r')
         # loop through each file
         for line in working_file:
-            skip +=1
-            if skip%2 == 1:
-                #found a match
-                hits+=1
-                elements = line.split("\t")
-                our_strand = elements[1]
+            #found a match
+            hits+=1
+            elements = line.split("\t")
+            our_strand = elements[1]
 
-                start_position = int(re.search('START=(.*)',elements[2]).group(1))
-                end_position = int(re.search('END=(.*)',elements[3]).group(1))
+            start_position = int(re.search('START=(.*)',elements[2]).group(1))
+            end_position = int(re.search('END=(.*)',elements[3]).group(1))
 
-                title = elements[0]
-                strand = ""
-                left_alignment = 0
-                right_alignment = 0
+            title = elements[0]
+            strand = ""
+            left_alignment = 0
+            right_alignment = 0
 
-                # regex search for actual alignment if complement
-                x = re.search('POSITION=COMPLEMENT\((.+?)\)DESCRIPTION', title)
-                if x:
-                    strand = '-'
-                    alignment = x.group(1)
-                    left_alignment = int(re.search('(.+?)\.\.', alignment).group(1))
-                    right_alignment = int(re.search('\.\.(.+?)', alignment).group(1))
-                else:
-                     strand = '+'
-                     # strand is not complement so regex search for + strand
-                     alignment = re.search('POSITION=(.+?)DESCRIPTION', title).group(1)
-                     left_alignment = int(re.search('(.+?)\.\.', alignment).group(1))
-                     right_alignment = int(re.search('\.\.(.*)', alignment).group(1))
+            # regex search for actual alignment if complement
+            x = re.search('POSITION=COMPLEMENT\((.+?)\)DESCRIPTION', title)
+            if x:
+                strand = '-'
+                alignment = x.group(1)
+                left_alignment = int(re.search('(.+?)\.\.', alignment).group(1))
+                right_alignment = int(re.search('\.\.(.+?)', alignment).group(1))
+            else:
+                 strand = '+'
+                 # strand is not complement so regex search for + strand
+                 alignment = re.search('POSITION=(.+?)DESCRIPTION', title).group(1)
+                 left_alignment = int(re.search('(.+?)\.\.', alignment).group(1))
+                 right_alignment = int(re.search('\.\.(.*)', alignment).group(1))
 
-                #calculate allowed mis-alignment
-                #print "our strand: " + str(our_strand) + " real strand: " + str(strand)
-                #print "our start: " + str(start_position) + " real start: " + str(left_alignment)
-                #print "our end: " + str(end_position) + " real end: " + str(right_alignment)
-                if our_strand == strand and left_alignment == start_position:
-                    # the alignment was right!
-                    true_positives += 1
-                else:
-                    # the alignment was wrong
-                    false_positives += 1
-                    # print "true: " + str(bowtie_position) + " low: " +str(shift_down) + " high: " + str(shift_up) + " predicted: " + str(bowtie_position)
+            #calculate allowed mis-alignment
+            #print "our strand: " + str(our_strand) + " real strand: " + str(strand)
+            #print "our start: " + str(start_position) + " real start: " + str(left_alignment)
+            #print "our end: " + str(end_position) + " real end: " + str(right_alignment)
+            if our_strand == strand and left_alignment == start_position:
+                # the alignment was right!
+                true_positives += 1
+            else:
+                # the alignment was wrong
+                false_positives += 1
+                # print "true: " + str(bowtie_position) + " low: " +str(shift_down) + " high: " + str(shift_up) + " predicted: " + str(bowtie_position)
         print TR
         print hits
-        false_negatives = TR - hits
-
+        #false_negatives = TR - hits
+        false_negatives = 0
+        #account for the tow different returned values
+        false_positives-=50
         #calculating precision and recall
         precision = 0
         recall = 0
@@ -77,6 +77,7 @@ for filename in os.listdir(directory):
         if recall + precision != 0:
             f_value = (2* recall * precision)/ (recall + precision)
         tableset.append((rd,true_positives*2,false_positives*2,false_negatives*2,f_value))
+
 
         print ">" + filename
         print "true positives: " + str(true_positives) + " false positives: " + str(false_positives) + " false negatives: " + str(false_negatives)
